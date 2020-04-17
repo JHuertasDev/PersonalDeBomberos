b4f875e1bd75a184ee0239c48b195ac1ea6c951c
JHuertasDev <jhuertasdeveloper@gmail.com>
2020-04-17 11:23:01 -0300

    Demo template

--- src/views/VistaPrincipal.js
+++ src/views/VistaPrincipal.js
@@ -1,23 +1,46 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path');
+
+const url = require('url');
 
 class VistaPrincipal{
+
+    getRutaSinglePageApplicationFile(file){
+        return path.resolve('./src/views/SinglePageApplication/'+file);
+    }
     mostrarVistaPrincipal(){
-        var vistaPrincipalObject = this;
+        var VistaPrincipalSelf = this;
 
         app.whenReady().then(function(){
             let win = new BrowserWindow({
                 width: 800,
                 height: 600,
                 webPreferences: {
-                nodeIntegration: true
-                }
+                    nodeIntegration: true
+                },
             });
+
+            var urlIndex = VistaPrincipalSelf.obtenerURLIndexHTML();
+            win.loadURL(urlIndex);
+
+            win.setMenuBarVisibility(false); //Ocultamos con las barras de navegacion
+            win.toggleDevTools();
+
         });
+        
+    }
+    obtenerURLIndexHTML(){
+        return this.obtenerURLDeLaVista(this.getRutaSinglePageApplicationFile('index.html'));
+    }
+    obtenerURLDeLaVista(ruta){
+        let urlArchivoHTML = url.format({
+            pathname: ruta,
+            protocol: 'file',
+            slashes: true
+        });
+        return urlArchivoHTML;
     }
 
-    crearVentanaPrincipal(){
-
-    }
 }
 
 module.exports = VistaPrincipal;
